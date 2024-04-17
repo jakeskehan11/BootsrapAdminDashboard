5809782fc595faabd49c65685ce6a3f0b63f8a93
jakeskehan11 <brojake87@gmail.com>
2024-04-17 14:14:43 +0800

    gggggg

---
+++ js/signin.js
@@ -0,0 +1,11 @@
+function validateForm() {
+   var username = document.getElementById("username").value;
+   var password = document.getElementById("password").value;
+
+   if (username.trim() === "" || password.trim() === "") {
+      alert("Please enter both username and password.");
+      return false;
+   }
+
+   return true; 
+}
